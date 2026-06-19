7999f82dfd11526b57bf6ecb999babab4b00e8d3
Fran <framosf2003@gmail.com>
2026-06-19 01:28:21 -0500

    Create Usuario.ts

---
+++ src/app/models/Usuario.ts
@@ -0,0 +1,12 @@
+export class Usuario{
+    idUsuario: number = 0;
+    nombreUsuario: string = '';
+    correoUsuario: string = '';
+    rolUsuario: string = '';
+    fechaRegistro:Date=new Date(); 
+    nacimientoUsuario: Date=new Date();
+    nacimientoAdolescente: Date=new Date();
+    generoAdolescente: string = '';
+    interesesAdolescente: string = '';
+    cantidadAdolescente: number = 0;
+}
