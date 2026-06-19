656bed8586fc69714953a9954c682e6bbc374a2f
Fran <framosf2003@gmail.com>
2026-06-19 01:27:57 -0500

    Create UsuarioInsertar.ts

---
+++ src/app/models/UsuarioInsertar.ts
@@ -0,0 +1,13 @@
+export class UsuarioInsertar{
+    idUsuario: number = 0;
+    nombreUsuario: string = '';
+    correoUsuario: string = '';
+    contrasenaUsuario: string = '';
+    rolUsuario: string = '';
+    fechaRegistro:Date=new Date(); 
+    nacimientoUsuario: Date=new Date();
+    nacimientoAdolescente: Date=new Date();
+    generoAdolescente: string = '';
+    interesesAdolescente: string = '';
+    cantidadAdolescente: number = 0;   
+}
